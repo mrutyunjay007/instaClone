ef3db17c6a6677ea35d70869e3934198165e6e14
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-02-09 22:56:22 +0530

    Setup firebse

---
+++ src/Firebase/config.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: String(import.meta.env.VITE_API_KEY),
+  authDomain: String(import.meta.env.VITE_AUTH_DOMAIN),
+  projectId: String(import.meta.env.VITE_PROJECT_ID),
+  storageBucket: String(import.meta.env.VITE_STORAGE_BUCKET),
+  messagingSenderId: String(import.meta.env.VITE_MESSAGING_SENDER_ID),
+  appId: String(import.meta.env.VITE_APP_ID),
+};
+
+export const app = initializeApp(firebaseConfig);
